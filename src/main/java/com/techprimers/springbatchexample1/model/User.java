0947f70f1707d64980a67cd0d5bcfdf2951ce8d3
JungGun <m78mjg@hotmail.com>
2022-09-20 16:25:08 +0900

    c

---
+++ src/main/java/com/techprimers/springbatchexample1/model/User.java
@@ -0,0 +1,56 @@
+package com.techprimers.springbatchexample1.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "\"user\"")
+public class User {
+    @Id
+    private Integer id;
+    private String name;
+    private String dept;
+    private Integer salary;
+    public User(Integer id, String name, String dept, Integer salary){
+        this.id = id;
+        this.name = name;
+        this.dept = dept;
+        this.salary = salary;
+    }
+    public User(){
+
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDept() {
+        return dept;
+    }
+
+    public void setDept(String dept) {
+        this.dept = dept;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
+}
